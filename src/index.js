3bd6ebcaa0abe872af0f40606b70788d48a64657
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-09 15:00:37 -0800

    Initialized constructor() in App component and added comments.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends Component {
-  render() {
+  // Initialize constructor(); old method of initializing state.
+  // constructor() is the first function to get called when a component loads, even before render().
+  // constructor() is the optimal place to call getCurrentPosition(), instead of double fetching user location from render().
+  constructor(props) {
+    super(props);
+
+    // Setting initial state; only time direct state assignment is allowed.
+    this.state = { lat: null };
+
+    // HOF to find current position.
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      // Callback to update state.
+      positionDetails => {
+        // Update state ONLY through setState(), never by direct assignment.
+        this.setState({ lat: positionDetails.coords.latitude });
+      },
+      // Callback to catch error, such as user location not found.
       err => console.log(err)
     );
-    return <div>Lattitude:</div>;
+  }
+
+  render() {
+    return <div>Lattitude: {this.state.lat}</div>;
   }
 }
 
