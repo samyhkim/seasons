24fcf6034e1125962451b182c4db17a6d16fa6c6
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-08 20:02:16 -0800

    Added Semantic-UI CDN and switched App component from functional to class.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return (
-    <div>
-      Hi there!
-      <SeasonDisplay />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    return <div>Lattitude:</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
