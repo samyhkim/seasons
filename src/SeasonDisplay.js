59000d84a178ea85b6404c4696023cf00757538a
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-09 19:16:34 -0800

    Updated SeasonDisplay component to display appropriate text and icons according to the user's location.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,9 +12,17 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
+  const text =
+    season === "summer" ? "Let's hit the beach!" : "Brr, it's chilly!";
+  const icon = season === "summer" ? "sun" : "snowflake";
 
-  return <div>Season display</div>;
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
