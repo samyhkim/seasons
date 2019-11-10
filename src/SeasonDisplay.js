eea48480f3011f6be4b8f30569a6dd5bdaf0f893
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-09 19:43:23 -0800

    Removed ternary expressions from SeasonDisplay component and instead created a seasonConfig object. Styled the pages of each season.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,19 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
+// Configuration object containing key-value pairs of season objects; allows us to avoid repetition
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Brr, it's chilly!",
+    iconName: "snowflake"
+  }
+};
+
+// Function to determine user's season
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     // Northern 3-8 = summer, else winter
@@ -11,16 +25,16 @@ const getSeason = (lat, month) => {
 };
 
 const SeasonDisplay = props => {
+  // Grab correct season of user by calling getSeason()
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-    season === "summer" ? "Let's hit the beach!" : "Brr, it's chilly!";
-  const icon = season === "summer" ? "sun" : "snowflake";
+  // Destructure season object's properties for easy access
+  const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${icon} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
